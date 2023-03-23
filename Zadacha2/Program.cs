cb1f7a8a890d885499e40e2fc809472fd72d94f5
School47 <School47@gmail.com>
2023-03-23 15:13:01 +0500

    zadacha2 reshena

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max, min;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");
